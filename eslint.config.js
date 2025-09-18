1dca2a64dfa817735e117c57bf7f5cbf702db88f
5anji <gitlanm@gmail.com>
2025-09-18 18:54:08 +0300

    commit exterior files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,20 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-
-})
+export default antfu(
+  {
+    typescript: true,
+    vue: true,
+  },
+  {
+    files: ['apps/server/**/*.{ts,js}'],
+    rules: {
+      'no-console': 'off', // allow console in server
+    },
+  },
+  {
+    files: ['apps/client/**/*.{ts,vue,js}'],
+    rules: {
+      'no-console': 'warn', // warn in client
+    },
+  },
+)
